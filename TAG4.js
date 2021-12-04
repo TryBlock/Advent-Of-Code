ae774c1734252eaed1cbd2548972641e9f2f1108
FlappyTop <chrissi.klaussxhulz@gmail.com>
2021-12-04 15:26:52 +0100

    Tag4

---
+++ TAG4.js
@@ -0,0 +1,18 @@
+const input = require("./input");
+const lines = input.getLines("./tag4.txt");
+
+const getRandomNumbers = () => {
+  return lines[0];
+};
+
+const getBoards = () => {
+  const boards = [];
+  let temp = [];
+  lines.slice(2).forEach((line) => {
+    
+  });
+  return boards;
+};
+
+console.log(getRandomNumbers());
+console.log(getBoards());
